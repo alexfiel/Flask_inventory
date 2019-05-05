52d6069c7d5058a5b47638ebb9bd6bf48d44b0cf
alexfiel <alexfiel07@gmail.com>
2019-05-05 20:28:02 +0800

    initial Commit

---
+++ inventory/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f62518f03b560013049fe43c83133a1c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:cto154@localhost:3306/inventory'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+
+from inventory import routes
