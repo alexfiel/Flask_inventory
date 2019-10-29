9ff16a321f70e3f17de8258f943171fc148af972
alexfiel07 <alexfiel07@gmail.com>
2019-10-29 16:00:35 +0800

    can now add new project

--- inventory/models.py
+++ inventory/models.py
@@ -22,7 +22,7 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     products = db.relationship('Product', backref='author', lazy=True)
-    projects = db.relationship('Project', backref='Project', lazy=True)
+    projects = db.relationship('Project', backref='Proj', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
